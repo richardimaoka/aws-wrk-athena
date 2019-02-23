d23b72132a96f4cf3dd6d535b27a1cbb62ce1faa
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-02-23 23:02:22 +0900

    fix STACK_NAME

--- local-commands.sh
+++ local-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WEB_SERVER_LOCAL_IP="10.0.0.212"
-STACK_NAME="aws-cloudformation-wrk.-1"
+STACK_NAME="aws-cloudformation-wrk-1"
 
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 set -e
