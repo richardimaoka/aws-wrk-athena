0715a8df67712a43a6095b99fff04df2cd149fef
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-02-25 01:29:57 +0900

    do not priing out curl stats

--- metadata-wrk.sh
+++ metadata-wrk.sh
@@ -6,14 +6,14 @@ set -e
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html#instancedata-data-retrieval
 # 169.254.169.254 is a special (loopback?) address for EC2 metadat
 
-AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id)
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
-HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/hostname)
-LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
-PUBLIC_IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id 2> /dev/null)
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
+INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type 2> /dev/null)
+HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/hostname 2> /dev/null)
+LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname 2> /dev/null)
+LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
+PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname 2> /dev/null)
+PUBLIC_IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4 2> /dev/null)
 
 echo "{ \
   \"metadata.wrk.ami_id\":          \"$AMI_ID\", \
