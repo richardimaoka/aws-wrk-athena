3e15a86c84e8d050b2e44b4dc0f4fa08daca4a38
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-02-25 04:04:09 +0900

    Loop over different parameters in local-main

--- local-main.sh
+++ local-main.sh
@@ -3,22 +3,15 @@
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 set -e
 
-# parse options, note that whitespace is needed (e.g. -c 4) between an option and the option argument
-#   Cloudformation related parameters:
-#    --test-exec-uuid    A UUID representing a group of Cloudformation stacks for test execution
-#    --test-seq-num      A sequence number within the test group represented by the UUID
-#    --wrk-local-ip      VPC-Local IP address for the wrk EC2 instance
-#    --web-local-ip      VPC-Local IP address for the web server EC2 instance
-#    --wrk-instance-type EC2 instance type for wrk
-#    --web-instance-type EC2 instance type for the web server
-#    --web-framework     web framework name to test
-
 UUID=$(uuidgen)
-./local-commands.sh \
-    --test-exec-uuid "${UUID}" \
-    --test-seq-num 1\
-    --wrk-local-ip "10.0.2.1" \
-    --web-local-ip "10.0.1.1" \
-    --wrk-instance-type "m5.xlarge" \
-    --web-instance-type "m5.xlarge" \
-    --web-framework "nginx"    
+for in $(jq -c '.[]' local-parameters.sample.json)
+do
+    ./local-commands.sh \
+        --test-exec-uuid "${UUID}" \
+        --test-seq-num "$(jq '.test_seq_num')"\
+        --wrk-local-ip "$(jq '.web_local_ip')" \
+        --web-local-ip "$(jq '.web_local_ip')" \
+        --wrk-instance-type "$(jq '.wrk_instance_type')" \
+        --web-instance-type "$(jq '.web_instance_type')" \
+        --web-framework "$(jq '.web_framework')"
+done
