33d253b627462c5f7fa40737ff0054c15c9bf8e1
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-03-05 05:39:07 +0900

    use -r to avoid aws cli command parse error in ip addresses

--- local-main.sh
+++ local-main.sh
@@ -22,8 +22,8 @@ do
     ./local-commands.sh \
         --test-exec-uuid "${UUID}" \
         --test-seq-num "$(echo ${params} | jq '.test_seq_num')"\
-        --wrk-local-ip "$(echo ${params} | jq '.wrk_local_ip')" \
-        --web-local-ip "$(echo ${params} | jq '.web_local_ip')" \
+        --wrk-local-ip "$(echo ${params} | jq -r '.wrk_local_ip')" \
+        --web-local-ip "$(echo ${params} | jq -r '.web_local_ip')" \
         --wrk-instance-type "$(echo ${params} | jq '.wrk_instance_type')" \
         --web-instance-type "$(echo ${params} | jq '.web_instance_type')" \
         --web-framework "$(echo ${params} | jq '.web_framework')" &
